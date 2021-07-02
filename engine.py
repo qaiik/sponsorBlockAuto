1843541e56c85c770de322eeca7336d7652d29e3
qaiik <82181231+qaiik@users.noreply.github.com>
2021-07-01 21:04:05 -0600

    Update engine.py

--- engine.py
+++ engine.py
@@ -3,4 +3,5 @@ from youtube_transcript_api import YouTubeTranscriptApi
 t = YouTubeTranscriptApi.get_transcript("QTk8Lx_Lpsk")
 
 for key in t:
-  
+  if 'sponsor' in dict[key]['text']:
+    print(dict[key]['text'])
